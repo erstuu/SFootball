ac34ac36b8230959900f293d9bdc7ae3e25c38ab
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-01-14 08:07:12 +0700

    feature/ implement repository for getting match from database

--- app/src/main/java/com/uas/sfootball/MatchRepository.kt
+++ app/src/main/java/com/uas/sfootball/MatchRepository.kt
@@ -2,10 +2,13 @@ package com.uas.sfootball
 
 import com.uas.sfootball.database.DateDao
 import com.uas.sfootball.database.MatchDao
+import com.uas.sfootball.models.Month
 
 class MatchRepository(private val matchDao: MatchDao, private val dateDao: DateDao) {
 
     fun getMatches(dateId: Int) = matchDao.getMatches(dateId)
+
+    fun getMatchesByDate(day: String, month: Month, year: String) = matchDao.getMatchesByDate(day, month, year)
 
     fun getDatesWithMatches() = dateDao.getDatesWithMatches()
 }
