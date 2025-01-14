7c8a47d180e25933b2d2b4d62180053c179904dc
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-01-14 13:35:40 +0700

    change directory

--- app/src/main/java/com/uas/sfootball/MatchRepository.kt
+++ app/src/main/java/com/uas/sfootball/MatchRepository.kt
@@ -2,6 +2,7 @@ package com.uas.sfootball
 
 import com.uas.sfootball.database.DateDao
 import com.uas.sfootball.database.MatchDao
+import com.uas.sfootball.models.MatchesWithDate
 import com.uas.sfootball.models.Month
 
 class MatchRepository(private val matchDao: MatchDao, private val dateDao: DateDao) {
@@ -11,4 +12,8 @@ class MatchRepository(private val matchDao: MatchDao, private val dateDao: DateD
     fun getMatchesByDate(day: String, month: Month, year: String) = matchDao.getMatchesByDate(day, month, year)
 
     fun getDatesWithMatches() = dateDao.getDatesWithMatches()
+
+    suspend fun addMatch(match: MatchesWithDate) {
+        matchDao.insertMatchWithDate(match)
+    }
 }
--- app/src/main/java/com/uas/sfootball/ViewModelFactory.kt
+++ app/src/main/java/com/uas/sfootball/ViewModelFactory.kt
@@ -2,13 +2,18 @@ package com.uas.sfootball
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.uas.sfootball.ui.HomeViewModel
+import com.uas.sfootball.ui.fragment.home.HomeViewModel
+import com.uas.sfootball.ui.fragment.tambahjadwal.TambahJadwalViewModel
 
 class ViewModelFactory(private val repository: MatchRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(HomeViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return HomeViewModel(repository) as T
+        }
+        if (modelClass.isAssignableFrom(TambahJadwalViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return TambahJadwalViewModel(repository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
