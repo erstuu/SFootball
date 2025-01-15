d2d78d2e17f98dd87de647e3ccf05d75eb43afb5
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-01-15 14:18:59 +0700

    add operation edit and delete

--- app/src/main/java/com/uas/sfootball/database/MatchDao.kt
+++ app/src/main/java/com/uas/sfootball/database/MatchDao.kt
@@ -1,6 +1,7 @@
 package com.uas.sfootball.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Transaction
@@ -21,6 +22,52 @@ interface MatchDao {
 
     @Insert
     suspend fun insertMatch(match: Match)
+
+    @Query("""
+        UPDATE dates 
+        SET day = :day, 
+            month = :month, 
+            year = :year, 
+            hour = :hour, 
+            minute = :minute, 
+            isClicked = :isClicked 
+        WHERE id = :id
+    """)
+    suspend fun updateDateById(
+        id: Int,
+        day: String,
+        month: Month,
+        year: String,
+        hour: String,
+        minute: String,
+        isClicked: Boolean
+    )
+
+    @Query("""
+    UPDATE matches 
+    SET nameHomeTeam = :nameHomeTeam, 
+        nameAwayTeam = :nameAwayTeam, 
+        logoHomeTeam = :logoHomeTeam, 
+        logoAwayTeam = :logoAwayTeam, 
+        stadium = :stadium, 
+        score = :score 
+    WHERE id = :id
+""")
+    suspend fun updateMatchById(
+        id: Int,
+        nameHomeTeam: String,
+        nameAwayTeam: String,
+        logoHomeTeam: String,
+        logoAwayTeam: String,
+        stadium: String,
+        score: String
+    )
+
+    @Query("SELECT * FROM dates WHERE id = :id")
+    suspend fun getDateById(id: Int): Dates?
+
+    @Query("SELECT * FROM matches WHERE dateId = :dateId")
+    suspend fun getMatchesByDateId(dateId: Int): List<Match>
 
     @Transaction
     suspend fun insertMatchWithDate(matchesWithDate: MatchesWithDate) {
@@ -51,4 +98,32 @@ interface MatchDao {
     WHERE matches.id = :id
     """)
     fun getMatchById(id: Int): Flow<MatchesWithDate>
+
+    @Transaction
+    suspend fun updateMatchesAndDateById(id: Int, matchesWithDate: MatchesWithDate) {
+
+        updateDateById(
+            id = id,
+            day = matchesWithDate.date.day,
+            month = matchesWithDate.date.month,
+            year = matchesWithDate.date.year,
+            hour = matchesWithDate.date.hour,
+            minute = matchesWithDate.date.minute,
+            isClicked = matchesWithDate.date.isClicked
+        )
+
+        updateMatchById(
+            id = id,
+            nameHomeTeam = matchesWithDate.matches[0].nameHomeTeam,
+            nameAwayTeam = matchesWithDate.matches[0].nameAwayTeam,
+            logoHomeTeam = matchesWithDate.matches[0].logoHomeTeam,
+            logoAwayTeam = matchesWithDate.matches[0].logoAwayTeam,
+            stadium = matchesWithDate.matches[0].stadium,
+            score = matchesWithDate.matches[0].score
+                ?: (matchesWithDate.date.hour + ":" + matchesWithDate.date.minute)
+        )
+    }
+
+    @Delete
+    suspend fun deleteMatch(match: Match)
 }
